897ba059fc9ef0195cc13fb5adf73979d250093f
FanchiniRudolf <fanchinirudolf@gmail.com>
2021-10-04 21:54:01 -0500

    made create and edit forms

--- gamification/src/Components/Create/Create.js
+++ gamification/src/Components/Create/Create.js
@@ -1,10 +1,71 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Form, Button,  } from 'react-bootstrap';
+import {useParams}  from "react-router-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function CreatePeriod() {
+
+  const { type } = useParams();
+  const [startDate, setStartDate] = useState(new Date());
+
+  console.log(typeof type);
+
+  const datePicker = (
+    <Form.Group className="mb-3" controlId="formTitle">
+        <Form.Label>Fecha de Fin</Form.Label>
+        <DatePicker /*TODO fix css*/selected={startDate} onChange={(date) => setStartDate(date)}>
+        </DatePicker>
+      </Form.Group>
+    )
+
+  const textQuestion = (tittle, desc) => {
+    return( 
+          <Form.Group className="mb-3" controlId="formTitle">
+            <Form.Label>{tittle}</Form.Label>
+            <Form.Control type="text" placeholder={desc} />
+          </Form.Group>
+        )
+  }
+
+
+   const renderElement = () => {
+    switch (type) {
+      case "course":
+        return textQuestion("Profesor que imparte", "Dame el Titulo")
+      
+      case "mission":
+        return datePicker
+        
+    case "period":
+        return datePicker
+
+    case "group":
+      return textQuestion("Nombre Materia", "Materia")
+
+    case "subject":
+      return;
+
+      default:
+        return <p>Error</p>
+    }
+  }
+
   return (
-    <div>
-      create period
-    </div>
+    <Form className="container-md text-center align-content-center">
+      <Form.Group className="mb-3 justify-content-center " controlId="formTitle">
+              <Form.Label>Titulo</Form.Label>
+              <Form.Control type="text" placeholder="Dame el Titulo" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formTitle">
+              <Form.Label>Descripción</Form.Label>
+              <Form.Control type="text" placeholder="Dame el Titulo" />
+      </Form.Group>
+      {renderElement()}
+      <Button variant="primary" type="submit">
+          Crear {type}
+      </Button>
+    </Form>
   )
 }
 
--- gamification/src/Components/Edit/Edit.js
+++ gamification/src/Components/Edit/Edit.js
@@ -1,10 +1,73 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Form, Button,  } from 'react-bootstrap';
+import {useParams}  from "react-router-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function EditCourse() {
+
+  const { type, id } = useParams();
+  //call id 
+  const info = {tittle:id, desc: "temp desc", date: new Date(), extra:"temp extra" }
+  const [startDate, setStartDate] = useState(new Date());
+
+  console.log(typeof type);
+
+  const datePicker = (
+    <Form.Group className="mb-3" controlId="formTitle">
+        <Form.Label>Fecha de Fin</Form.Label>
+        <DatePicker /*TODO fix css*/selected={info.date} onChange={(date) => setStartDate(date)}>
+        </DatePicker>
+      </Form.Group>
+    )
+
+  const textQuestion = (tittle, desc) => {
+    return( 
+          <Form.Group className="mb-3" controlId="formTitle">
+            <Form.Label>{tittle}</Form.Label>
+            <Form.Control type="text" placeholder={desc} />
+          </Form.Group>
+        )
+  }
+
+
+   const renderElement = () => {
+    switch (type) {
+      case "course":
+        return textQuestion("Profesor que imparte", info.extra)
+      
+      case "mission":
+        return datePicker
+        
+    case "period":
+        return datePicker
+
+    case "group":
+      return textQuestion("Nombre Materia", info.extra)
+
+    case "subject":
+      return;
+
+      default:
+        return <p>Error</p>
+    }
+  }
+
   return (
-    <div>
-      edit course
-    </div>
+    <Form className="container-md text-center align-content-center">
+      <Form.Group className="mb-3 justify-content-center " controlId="formTitle">
+              <Form.Label>Titulo</Form.Label>
+              <Form.Control type="text" placeholder={info.tittle} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formTitle">
+              <Form.Label>Descripción</Form.Label>
+              <Form.Control type="text" placeholder={info.desc} />
+      </Form.Group>
+      {renderElement()}
+      <Button variant="primary" type="submit">
+          Editar {type}
+      </Button>
+    </Form>
   )
 }
 
