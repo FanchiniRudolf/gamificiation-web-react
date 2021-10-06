a9e33c7a8fd5c1449a656fb3c88804e0597a0d94
FanchiniRudolf <fanchinirudolf@gmail.com>
2021-10-05 19:36:29 -0500

    added intuitive ux changes for mission grade with hooks

--- gamification/src/Components/GradeStudent/GradeStudent.js
+++ gamification/src/Components/GradeStudent/GradeStudent.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 
 function GradeStudent() {
+
+  const [mission, setMission] = useState({hp:100, xp:50, coins:20});
+  const [hp, setHp] = useState(0);
+  const [xp, setXp] = useState(0);
+  const [coins, setCoins] = useState(0);
+  const [grade, setGrade] = useState(100);
+
+
   return (
     <div>
       <Container>
@@ -21,8 +29,6 @@ function GradeStudent() {
             <Col lg={6}>
               <h3>Misión</h3>
 
-              {/* TODO: use hook that changes original value of the mission given
-              so it can be changed based on merit, without professor needing to lookup value */}
               <Form.Select aria-label="Default select example">
                 <option>Misión a calificar</option>
                 <option value="1">One</option>
@@ -34,38 +40,47 @@ function GradeStudent() {
           
           <Row className="mt-3">
             <Col lg={6}>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label><h5>HP</h5></Form.Label>
-                {/* TODO add a defalut/starting value */}
-                <Form.Control type="number" placeholder="HP a sumar o restar" />
+              <Form.Group className="mb-3" controlId="hp">
+                <Form.Label><h5>HP a sumar o restar</h5></Form.Label>
+                <Form.Control type="number" placeholder={hp}  onChange={(e) => {setHp(Number(e.target.value))}}/>
+                <Form.Text className="text-muted">
+                  HP Final: {hp + (mission.hp)*(grade/100)}
+                </Form.Text>
               </Form.Group>
             </Col>
           </Row>
           <Row className="mt-3">
             <Col lg={6}>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label><h5>Experiencia</h5></Form.Label>
-                {/* TODO add a defalut/starting value */}
-                <Form.Control type="number" placeholder="XP a sumar o restar" />
+              <Form.Group className="mb-3" controlId="xp">
+                <Form.Label><h5>XP a sumar o restar</h5></Form.Label>
+                <Form.Control type="number" placeholder={xp}  onChange={(e) => {setXp(Number(e.target.value))}}/>
+                <Form.Text className="text-muted">
+                  XP Final: {xp + (mission.xp)*(grade/100)}
+                </Form.Text>
               </Form.Group>
             </Col>
           </Row>
           <Row className="mt-3 mb-5">
             <Col lg={6}>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label><h5>Monedas</h5></Form.Label>
-                {/* TODO add a defalut/starting value */}
-                <Form.Control type="number" placeholder="Monedas a sumar o restar" />
+              <Form.Group className="mb-3" controlId="coins">
+              <Form.Label><h5>Monedas a sumar o restar</h5></Form.Label>
+                <Form.Control type="number" placeholder={coins}  onChange={(e) => {setCoins(Number(e.target.value))}}/>
+                <Form.Text className="text-muted">
+                  Monedas Finales: {coins + (mission.coins)*(grade/100)}
+                </Form.Text>
               </Form.Group>
             </Col>
           </Row>
 
           <Row className="mt-3 mb-5">
             <Col lg={6}>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Group className="mb-3" controlId="grade">
                 <Form.Label><h5>Calificación</h5></Form.Label>
-                {/* TODO add a defalut/starting value */}
-                <Form.Control type="number" placeholder="La calificación numérica (0 - 100) de esta tarea" />
+                <Form.Control type="number" value={grade} 
+                  onChange={(e) => {setGrade(Number(e.target.value))}}/>
+              <Form.Text className="text-muted">
+                La calificación numérica (0 - 100) de esta tarea
+              </Form.Text>
               </Form.Group>
             </Col>
           </Row>
