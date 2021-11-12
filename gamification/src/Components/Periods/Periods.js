a6f504ea26216587877141db681eda3bc78dcd7f
Roberto Tellez <A01374866@itesm.mx>
2021-11-11 21:16:15 -0600

    Implement GET All periods of the user
    Missing to render info

--- gamification/src/Components/Periods/Periods.js
+++ gamification/src/Components/Periods/Periods.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import { getCookie } from '../../Functions/Cookies';
 
+import {useFetch} from "../../Hooks/useFetch"
 import './Periods.css';
-//@bobby
+
 function Periods() {
+
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
+  const { loading, info } = useFetch(API_BASE_URL + "periods",
+    "GET",
+    {"Authorization": getCookie("session_token")})
+
+  let periodTable;
+  if (loading === null) {
+    periodTable = <div></div>
+  } else if (loading === true) {
+    periodTable = <p>Loading...</p>
+  } else if (loading === false) {
+    periodTable = <p>List goes here</p>
+    // periodTable = info.map(periodInfo => )
+    console.log(info)
+  }
+
   return (
-    // <div className="StudentGroup">
     <div>
       <Container>
       <Row className="mt-5 mb-3">
@@ -16,6 +34,12 @@ function Periods() {
             <Button variant="primary" size="md" href="/create/period">
               Crear
             </Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col lg={12}>
+            {periodTable}
           </Col>
         </Row>
       </Container>
